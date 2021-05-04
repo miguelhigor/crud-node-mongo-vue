1a7be2e0cab91537a2512030d1ab39c548ea44de
Higor Miguel <higor.mig@gmail.com>
2021-05-03 21:58:28 -0300

    Environment variables mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose"
 
-const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/QUALICORP";
+// const url = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/QUALICORP";
+const url = process.env.MONGO_URI || "mongodb+srv://higor:higor@cluster0.kwl6x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
